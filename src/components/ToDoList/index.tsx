f91b2603b47548cddf97240a8bdf545ed74e1d09
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-11-09 20:00:06 -0300

    chore: add animation to to.do item

--- src/components/ToDoList/index.tsx
+++ src/components/ToDoList/index.tsx
@@ -1,21 +1,30 @@
-import { ToDoItem } from '@components/ToDoItem';
-import { ToDo } from '@context/ToDo/context';
 import React from 'react';
-import { FlatList } from 'react-native';
+import { AnimatePresence, MotiView } from 'moti';
+import { ScrollView, Dimensions } from 'react-native';
+
 import { useToDos } from '@hooks/useToDos';
 
-// import { Container } from './styles';
+import { ToDoItem } from '@components/ToDoItem';
+
+const deviceWidth = Dimensions.get('screen').width;
 
 export function ToDoList() {
   const { toDos } = useToDos();
 
   return (
-    <FlatList<ToDo>
-      data={toDos}
-      keyExtractor={(toDo) => toDo.id}
-      renderItem={({ item: toDo, index }) => (
-        <ToDoItem showGradient={index % 2 === 0} data={toDo} />
-      )}
-    />
+    <ScrollView>
+      <AnimatePresence>
+        {toDos.map((toDo, index) => (
+          <MotiView
+            key={toDo.id}
+            from={{ translateX: deviceWidth, opacity: 0 }}
+            animate={{ translateX: 0, opacity: 1 }}
+            exit={{ translateX: -deviceWidth, opacity: 0 }}
+          >
+            <ToDoItem key={toDo.id} showGradient={index % 2 === 0} data={toDo} />
+          </MotiView>
+        ))}
+      </AnimatePresence>
+    </ScrollView>
   );
 }
