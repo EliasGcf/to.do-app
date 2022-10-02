fcae2172e4a507ab4436a1d14d058759518d59ad
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-10-02 14:50:55 -0300

    chore: show total todos and done todos

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -9,11 +9,13 @@ import { useTodos } from '~/hooks/useTodos';
 export function TodoList() {
   const todos = useTodos((state) => state.todos);
 
+  const doneTodos = todos.filter((todo) => todo.done);
+
   return (
     <View>
       <View className="flex-row justify-between mt-8 mb-5">
-        <Label variant="created" count={0} />
-        <Label variant="completed" count={0} />
+        <Label variant="created" count={todos.length} />
+        <Label variant="completed" count={doneTodos.length} />
       </View>
 
       <FlatList
@@ -21,9 +23,9 @@ export function TodoList() {
         className="h-full"
         ListEmptyComponent={EmptyList}
         keyExtractor={(item) => item.id}
-        renderItem={({ item, index }) => (
-          <TodoItem data={item} className={index !== 0 ? 'mt-2' : ''} />
-        )}
+        renderItem={({ item, index }) => {
+          return <TodoItem data={item} className={index !== 0 ? 'mt-2' : ''} />;
+        }}
       />
     </View>
   );
